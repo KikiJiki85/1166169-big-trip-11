7ba870b3de1ef1cc5d353f40b051ad3cef276efb
Aleksey Vaskin <alzor85@gmail.com>
2020-05-18 07:47:38 +0300

    Реализует показ статистики часть 1 WIP

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import TripMenuComponent from "./components/trip-menu.js";
 import NoPointsComponent from "./components/no-points.js";
 import TripCostComponent from "./components/trip-cost.js";
 import TripInfoComponent from "./components/trip-info.js";
+import StatisticsComponent from "./components/statistics.js";
 
 import TripController from "./controllers/trip.js";
 import FilterController from "./controllers/filter.js";
 
 import {render, RenderPosition} from "./utils/render.js";
-import {menuItems} from "./mock/menu.js";
+import {menuItems, MenuItem} from "./mock/menu.js";
 import {cards} from "./mock/card.js";
 
 import PointsModel from "./models/point.js";
@@ -19,15 +20,16 @@ const tripInfoElement = tripMainElement.querySelector(`.trip-info`);
 const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
 const tripDaysComponent = new TripDaysContainer();
 
+const siteMainElement = document.querySelector(`.page-body__page-main`);
+const menuComponent = new TripMenuComponent(menuItems);
+const statisticsComponent = new StatisticsComponent();
+
 
 const pointsModel = new PointsModel();
 pointsModel.setPoints(cards);
+const tripController = new TripController(tripDaysComponent, pointsModel);
 
-render(
-    tripControlsElement,
-    new TripMenuComponent(menuItems),
-    RenderPosition.AFTERBEGIN
-);
+render(tripControlsElement, menuComponent, RenderPosition.AFTERBEGIN);
 
 const filterController = new FilterController(tripControlsElement, pointsModel);
 filterController.render();
@@ -57,7 +59,6 @@ if (cards.length === 0) {
       RenderPosition.AFTERBEGIN
   );
 
-  const tripController = new TripController(tripDaysComponent, pointsModel);
   tripController.render(cards);
 
   document
@@ -67,3 +68,21 @@ if (cards.length === 0) {
     });
 
 }
+
+render(siteMainElement, statisticsComponent);
+statisticsComponent.hide();
+
+menuComponent.setChangeHandler((menuItem) => {
+  switch (menuItem) {
+    case MenuItem.TABLE:
+      menuComponent.setActiveItem(MenuItem.TABLE);
+      tripController.show();
+      statisticsComponent.hide();
+      break;
+    case MenuItem.STATS:
+      menuComponent.setActiveItem(MenuItem.STATS);
+      statisticsComponent.show();
+      tripController.hide();
+      break;
+  }
+});
